517409b3b5c888c9298a1da8d88a08d29b1ea24a
ro <ro.michi1129@gmail.com>
2021-09-26 13:32:41 +0900

    add components

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core';
+import MessageList from './MessageList';
+import MessageInputField from './MessageInputField';
 
-const Main = () => {
+const useStyles = makeStyles({
+  root: {
+    display: 'grid',
+    height: '100vh',
+    gridTemplateRows: '1fr auto',
+  }
+});
+
+const Main = ({ name }) => {
+  const classes = useStyles();
+
   return (
-    <div>Main</div>
+    <div className={classes.root}>
+      <MessageList />
+      <MessageInputField name={name} />
+    </div>
   );
 };
 
