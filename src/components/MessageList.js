6f92fe87b9ed873fcc3ab02f614f4a9ae1fe2928
ro <ro.michi1129@gmail.com>
2021-09-26 17:34:41 +0900

    read list

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,5 +1,7 @@
 import { makeStyles } from "@material-ui/core";
-import React from "react";
+import React, { useState } from "react";
+import { messagesRef } from "../firebase";
+import { query, onValue, orderByKey, limitToLast } from 'firebase/database';
 
 const useStyles = makeStyles({
   root: {
@@ -8,7 +10,25 @@ const useStyles = makeStyles({
 })
 
 const MessageList = () => {
+  const [messages, setMessages] = useState([]);
+
   const classes = useStyles();
+
+  const queryRef = query(messagesRef, orderByKey(), limitToLast(3));
+
+  onValue(queryRef, (snapshot) => {
+
+    snapshot.forEach((childSnapshot) => {
+      const childKey = childSnapshot.key;
+      const childData = childSnapshot.val();
+      messages.push({ ...childData, key: childKey });
+      setMessages(messages);
+    });
+
+  }, {
+    onlyOnce: true
+  });
+
 
   return (
     <div className={classes.root}>MessageList</div>
