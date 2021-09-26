094509a555b919489437da1c59fedf3dc22a4548
ro <ro.michi1129@gmail.com>
2021-09-26 18:23:23 +0900

    show message list

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,38 +1,55 @@
-import { makeStyles } from "@material-ui/core";
-import React, { useState } from "react";
+import { List, makeStyles } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
 import { messagesRef } from "../firebase";
 import { query, onValue, orderByKey, limitToLast } from 'firebase/database';
+import MessageItem from "./MessageItem";
 
 const useStyles = makeStyles({
   root: {
     gridRow: 1,
+    width: '100%',
+    overflow: 'auto',
   }
 })
 
 const MessageList = () => {
+
   const [messages, setMessages] = useState([]);
 
   const classes = useStyles();
 
-  const queryRef = query(messagesRef, orderByKey(), limitToLast(3));
+  const queryRef = query(messagesRef, orderByKey(), limitToLast(15));
 
-  onValue(queryRef, (snapshot) => {
+  useEffect(() => {
 
-    snapshot.forEach((childSnapshot) => {
-      const childKey = childSnapshot.key;
-      const childData = childSnapshot.val();
-      messages.push({ ...childData, key: childKey });
-      setMessages(messages);
+    onValue(queryRef, (snapshot) => {
+
+      const newMessages = [...messages];
+      snapshot.forEach((childSnapshot) => {
+        const childKey = childSnapshot.key;
+        const childData = childSnapshot.val();
+        newMessages.push({ ...childData, key: childKey });
+      });
+
+      setMessages(newMessages);
+
+    }, {
+      onlyOnce: true
     });
 
-  }, {
-    onlyOnce: true
-  });
-
+  }, []);
 
   return (
-    <div className={classes.root}>MessageList</div>
-  )
+    <List className={classes.root}>
+      {
+        messages.map(({ key, name, text }) => {
+          return (
+            <MessageItem key={key} name={name} text={text} />
+          )
+        })
+      }
+    </List >
+  );
 };
 
 export default MessageList;
